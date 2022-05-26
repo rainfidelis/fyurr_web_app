9b41f5a75789b9e0a28e961052aa524e68115936
rainfidelis <rainnyfidelis@yahoo.com>
2022-05-26 04:36:14 +0100

    altered sqlalchemy dialect name for heroku

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ DEBUG = config('DEBUG')
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+
+# Alter Postgresql dialect name in heroku
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Keep CSRF token till end of current session
