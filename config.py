553534810b18ee989fe33854d7d1aa798b632876
rainfidelis <rainnyfidelis@yahoo.com>
2022-05-26 04:20:30 +0100

    adjusted config variables

--- config.py
+++ config.py
@@ -6,12 +6,11 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-# DEBUG = True
-DEBUG = False
+
+DEBUG = config('DEBUG')
 
 # TODO IMPLEMENT DATABASE URL
-# SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
-SQLALCHEMY_DATABASE_URI = 'postgres://tfdifunehnugpr:674d906ea3e40d58d0dbbe19501739e541e9bd7cd30a07a06b6544f43fb4209f@ec2-54-204-56-171.compute-1.amazonaws.com:5432/d47lkklk22aqfk'
+SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Keep CSRF token till end of current session
