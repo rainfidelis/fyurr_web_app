e49b6354029a3d78df6981f07d16b6ebbf7150ca
rainfidelis <rainnyfidelis@yahoo.com>
2022-05-26 03:55:45 +0100

    minor heroku tweak

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgres://tfdifunehnugpr:674d906ea3e40d58d0dbbe19501739e541e9bd7cd30a07a06b6544f43fb4209f@ec2-54-204-56-171.compute-1.amazonaws.com:5432/d47lkklk22aqfk'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Keep CSRF token till end of current session
